8b48acda529ae110f66d6825313654b5e0adf7a4
koljakiss <140483096+kdkiss@users.noreply.github.com>
2024-08-26 15:37:44 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 setup(
     name='mkdocs-bootstrap-elements-plugin',
-    version='0.1.0',
+    version='0.1.6',  # Increment the version number
     description='A MkDocs plugin that adds Bootstrap accordions, modals, and cards.',
     long_description='This plugin allows you to easily add Bootstrap accordions, modals, and cards to your MkDocs site using custom Markdown syntax.',
+    long_description_content_type='text/markdown',
     keywords='mkdocs python markdown bootstrap',
     url='https://github.com/kdkiss/mkdocs-bootstrap-elements-plugin',
     author='Your Name',
@@ -26,10 +27,18 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    packages=find_packages(),
+    packages=find_namespace_packages(include=['mkdocs_bootstrap_elements_plugin*']),
+    include_package_data=True,
+    package_data={
+        'mkdocs_bootstrap_elements_plugin': [
+            'css/*.css',
+            'js/*.js'
+        ],
+    },
     entry_points={
         'mkdocs.plugins': [
             'bootstrap_elements = mkdocs_bootstrap_elements_plugin:BootstrapElementsPlugin',
         ]
-    }
+    },
+    zip_safe=False
 )
